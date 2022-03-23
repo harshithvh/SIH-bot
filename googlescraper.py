188af92dda2d285aa73e50df924b804ebc18461a
Harshith VH <vhharshith77@gmail.com>
2022-03-23 11:29:53 +0530

    v12

--- googlescraper.py
+++ googlescraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 
 
 import time
