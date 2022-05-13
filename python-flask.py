c0f74cb0672161b432c4370f7a8bb7298a5cb85c
Harshith VH <vhharshith77@gmail.com>
2022-05-13 18:40:13 +0530

    multilingual supported

--- python-flask.py
+++ python-flask.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
+from deep_translator import GoogleTranslator, single_detection
 from twilio.twiml.messaging_response import MessagingResponse
+from xml.dom import minidom
+from bs4 import BeautifulSoup
 import googlescraper
 
 app = Flask(__name__)
@@ -20,9 +23,32 @@ def index():
 @app.route('/query', methods=['POST'])
 def response():
     msg = request.form.get('Body')
+    lang = single_detection(msg, api_key='bbe1af5f4651c0d5268d420bcfb2b36d')
+    translated = GoogleTranslator(source="auto", target="en").translate(msg)
+    print(lang, translated)
     resp = MessagingResponse()
-    resp.message(googlescraper.search(msg))
-    return str(resp)
+    resp.message(googlescraper.search(translated))
+
+    bs = BeautifulSoup(str(resp), 'xml')
+
+    uni = bs.find_all('Message')
+
+    for uniq in uni:
+        res = uniq.get_text()
+    
+    result = GoogleTranslator(source="en", target=lang).translate(res)
+    print(result)
+
+    root = minidom.Document()
+
+    xml = root.createElement('Response')
+    root.appendChild(xml)
+
+    productChild = root.createElement('Message')
+    productChild.appendChild(root.createTextNode(result))
+    xml.appendChild(productChild)
+
+    return root.toprettyxml()
     
 
 if __name__=='__main__':
@@ -36,5 +62,6 @@ Step3: Under Twilio Sandbox for WhatsApp > add the URL given by ngrok (Ex: https
 Step4: Run python-flask.py
 Step5: Go to downloads > ngrok > run the ngrok.exe file
 Step6: In the ngrok.exe enter 'ngrok http 5000' to generate the URL
+<?xml version="1.0" encoding="UTF-8"?><Response><Message>Hello, how can I help?</Message></Response>
 '''
 
